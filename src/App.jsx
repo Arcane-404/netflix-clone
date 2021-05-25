fd8241fa4da09bf1e7819598f725b4a7eed9ddb8
Herman Liu <hermanliu95@gmail.com>
2021-05-25 14:38:04 -0700

    added logo and signin button that links to login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 // import { image1 } from '/public/image'
 // import image1 from '/public/image/initial-setup.png'
 import { Switch, Route } from 'react-router-dom'
-import { LoginPage, SignupPage } from './pages/'
+import { HomePage, LoginPage, SignupPage } from './pages/'
 
 const App = () => {
 	return (
 		<>
 			{/* <img src={ image1 } alt="the-test-image" /> */}
 			<Switch>
-				<Route exact path="/"></Route>
+				<Route exact path="/"> <HomePage/> </Route>
 				<Route path="/login"> <LoginPage/> </Route>
 				<Route path="/signup"> <SignupPage/> </Route>
 				<Route path="/browse"> </Route>
