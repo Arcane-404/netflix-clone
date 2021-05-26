3e90a289939407f4928aab3b969984dbc82a7b16
Herman Liu <hermanliu95@gmail.com>
2021-05-26 13:15:47 -0700

    feat: successfully route back to home page from signin page

---
+++ src/components/SigninForm/_SigninForm.styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	width: 100%;
+`
+
+//Form, Title, Input, button, content, signup
+
+export const Inner = styled.form`
+	width: 90%;
+	max-width: 500px;
+	margin: 0 auto;
+	display: flex;
+	flex-direction: column;
+	border: 1px solid black;
+`
+
+export const Title = styled.h1``
+
+export const Input = styled.input``
+
+export const Button = styled.button``
+
+export const Frame = styled.div`
+	display: flex;
+`
+
+export const Content = styled.p``
+
+export const Link = styled.p``
--- src/pages/_LoginPage.js
+++ src/pages/_LoginPage.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { NavBar, SigninForm } from '../components'
+
+const logo = {
+	img: '/images/logo.svg'
+}
 
 const LoginPage = () => {
 	return (
 		<>
-			<h1>Login Page</h1>
+			<NavBar>
+				<NavBar.Inner>
+					<Link to="/">
+						<NavBar.Logo src={logo.img}/>
+					</Link>
+				</NavBar.Inner>
+			</NavBar>
+
+			<SigninForm>
+				<SigninForm.Inner>
+
+					<SigninForm.Title>Sign In</SigninForm.Title>
+					<SigninForm.Input type="text" placeholder="Email or phone number"/>
+					<SigninForm.Input type="password" placeholder="Password"/>
+					<SigninForm.Button> Sign In </SigninForm.Button>
+
+					<SigninForm.Frame>
+						<SigninForm.Content>
+							New to Netflix?
+						</SigninForm.Content>
+						<Link to="/">
+							<SigninForm.Link>Sign up now</SigninForm.Link>
+						</Link>
+					</SigninForm.Frame>
+
+				</SigninForm.Inner>
+			</SigninForm>
 		</>
 	)
 }
