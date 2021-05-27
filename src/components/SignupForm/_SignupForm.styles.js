3cb5db2086c874e5a6eae16669bb4e232670e0e0
Herman Liu <hermanliu95@gmail.com>
2021-05-27 15:07:29 -0700

    herm: add signup form

---
+++ src/components/SignupForm/_SignupForm.styles.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	border: 1px solid black;
+	margin: 0 auto;
+	width: 90%;
+	max-width: 800px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+
+export const Inner = styled.div``
+
+export const Title = styled.h1`
+	font-size: 1rem;
+	width: 30ch;
+	text-align: center;
+
+	@media (min-width: 750px) {
+		width: 100%;
+	}
+`
+
+export const Form = styled.form`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+
+	@media (min-width: 750px) {
+		flex-direction: row;
+		height: 50px;
+		margin-bottom: 2rem;
+	}
+
+`
+
+export const Input = styled.input`
+	width: 100%;
+	height: 50px;
+	padding: 0 1rem;
+
+	@media (min-width: 750px) {
+		width: 600px;
+	}
+`
+
+export const Button = styled.button`
+	padding: 10px 20px;
+	background: red;
+	color: white;
+	border-radius: 0.25rem;
+	margin: 1rem auto;
+
+	@media (min-width: 750px){
+		margin: 0;
+		width: 40%;
+		font-size: 1.75rem;
+		border-top-left-radius: 0;
+		border-bottom-left-radius: 0;
+	}
+`
+
---
+++ src/containers/index.jsx
@@ -0,0 +1,1 @@
+export { default as SignupFormContainer } from './_SignupFormContainer'
