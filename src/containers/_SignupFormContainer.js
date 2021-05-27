661a868d7c9df8504bcf20567d5b40eca620f5c2
Herman Liu <hermanliu95@gmail.com>
2021-05-27 16:23:20 -0700

    herm/wip/sucessful-route-to-signup

--- src/containers/_SignupFormContainer.js
+++ src/containers/_SignupFormContainer.js
@@ -1,20 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { SignupForm, Icon } from '../components'
 
 const SignupFormContainer = () => {
+
+	const [ email, setEmail ] = useState('')
+
+	const handleChange = (e) => {
+		setEmail(e.target.value)
+	}
+
+	const history = useHistory()
+
+	const formSubmit = () => {
+		history.push('/signup')
+	}
+
 	return (
 		<SignupForm>
 			<SignupForm.Title>
 				Ready to watch? Enter your email to create or restart your membership.
 			</SignupForm.Title>
-			<SignupForm.Form>
-				<SignupForm.Input type="text" placeholder="Email address"/>
+
+			<SignupForm.Form onSubmit={ formSubmit }>
+				<SignupForm.Input
+					type="text"
+					placeholder="Email address"
+					value={email}
+					onChange={ handleChange }
+				/>
 				<SignupForm.Button>
-					Get Started
-					<Icon.FaChevronRight />
+					Get Started <Icon.FaChevronRight />
 				</SignupForm.Button>
 			</SignupForm.Form>
+
 		</SignupForm>
 	)
 }
