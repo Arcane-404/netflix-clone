bb4b4ef82342e84aff1cb357700cee203eac56b1
Adnan Niaz <adnanniaz77@yahoo.com>
2021-05-27 19:27:15 -0700

    feat: Create CardsWrapper

---
+++ src/components/Card/_Card.styles.js
@@ -0,0 +1,6 @@
+// export const CardWrapper
+// export const CardImage
+// export const CardTitle
+// export const CardSubitle
+// export const CardButton
+// export const CardTag
--- src/pages/_BrowsePage.jsx
+++ src/pages/_BrowsePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { CardsContainer } from '../containers/'
 
 const BrowsePage = () => {
 	return (
 		<>
 			<h1> Browse Page </h1>
+			<CardsContainer />
 		</>
 	)
 }
