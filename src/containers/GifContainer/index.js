1abda492760c8439e56fa17f3f12faff6b56af72
Herman Liu <hermanliu95@gmail.com>
2021-06-05 22:40:02 -0700

    fix merge conflict

---
+++ src/containers/GifContainer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Gif } from '../../components'
+
+const GifContainer = () => {
+	return (
+		<Gif>
+			<Gif.Img src="/images/netflix-logo-animation.gif" />
+		</Gif>
+	)
+}
+
+export default GifContainer
