92964a8f55a7898b777439aaa389cc411d57465c
awwmicky <michael_fred_alvarez@yahoo.com>
2021-06-07 16:46:49 -0700

    feat: adjust useFuse hook

--- src/hooks/_useFuse.js
+++ src/hooks/_useFuse.js
@@ -1,22 +1,24 @@
 import { useCallback, useMemo, useEffect, useState } from 'react'
 import Fuse from 'fuse.js'
 
-const useFuse = (data, options) => {
+const initialOptions = {
+	limit: undefined
+}
+
+const useFuse = (data, options = initialOptions) => {
 	const [ query, setQuery ] = useState('')
 	const [ results, setResults ] = useState([])
 
-	const { limit, matchAllOnEmptyQuery, ...fuseOptions } = options
+	const { limit, ...fuseOptions } = options
 	const fuse = useMemo(() => new Fuse(data, fuseOptions), [ data, fuseOptions ])
 	const handleSearch = useCallback(e => setQuery(e.target.value), [ setQuery ])
 
 	useEffect(() => {
-		// console.log('data', data)
 		if (!data) return
-		const fuzzy = query && fuse.search(query).map(result => result.item)
+		const fuzzy = query && fuse.search(query, { limit }).map(result => result.item)
 		if (fuzzy.length !== results.length && query.length > 3) return setResults(fuzzy)
 		if (!query && !fuzzy.length) return setResults(data)
-		// }
-	}, [ data, fuse, query, results, fuseOptions ])
+	}, [ data, fuse, query, limit, results, fuseOptions ])
 
 	return {
 		search: query,
