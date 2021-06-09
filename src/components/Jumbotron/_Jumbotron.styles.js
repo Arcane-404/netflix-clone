0446ebf01fb79427c8c06462bb04733ab39b374f
Herman Liu <hermanliu95@gmail.com>
2021-06-08 20:57:01 -0700

    create accordion component

---
+++ src/components/Jumbotron/_Jumbotron.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``
+
+export const Inner = styled.div``
+
+export const ContentFrame = styled.div``
+
+export const Title = styled.h1``
+
+export const Subtitle = styled.h2``
+
+export const ImageFrame = styled.div``
+
+export const Image = styled.img``
+
---
+++ src/components/Jumbotron/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Container, Inner, ContentFrame, Title, Subtitle, ImageFrame, Image } from './_Jumbotron.styles'
+
+export default function Jumbotron ({ children, ...restProps }) {
+	return <Container { ...restProps }> { children } </Container>
+}
+
+Jumbotron.Inner = function JumbotronInner ({ children, ...restProps }) {
+	return <Inner { ...restProps }> { children } </Inner>
+}
+
+Jumbotron.ContentFrame = function JumbotronContentFrame ({ children, ...restProps }) {
+	return <ContentFrame { ...restProps }> { children } </ContentFrame>
+}
+
+Jumbotron.Title = function JumbotronTitle ({ children, ...restProps }) {
+	return <Title { ...restProps }> { children } </Title>
+}
+
+Jumbotron.Inner = function JumbotronInner ({ children, ...restProps }) {
+	return <Inner { ...restProps }> { children } </Inner>
+}
+
+Jumbotron.Subtitle = function JumbotronSubtitle ({ children, ...restProps }) {
+	return <Subtitle { ...restProps }> { children } </Subtitle>
+}
+
+Jumbotron.ImageFrame = function JumbotronImageFrame ({ children, ...restProps }) {
+	return <ImageFrame { ...restProps }> { children } </ImageFrame>
+}
+
+Jumbotron.Image = function JumbotronImage ({ children, ...restProps }) {
+	return <Image { ...restProps } />
+}
