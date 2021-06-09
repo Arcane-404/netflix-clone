fc1989379f089fda0a38dbd3072ea20664d99fca
Herman Liu <hermanliu95@gmail.com>
2021-06-08 21:03:10 -0700

    create accordion compoennt

---
+++ src/components/Accordion/_Accordion.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``
+
+export const Inner = styled.div``
+
+export const Frame = styled.div``
+
+export const Header = styled.div``
+
+export const Title = styled.h1``
+
+export const Toggle = styled.i``
+
+export const Body = styled.div``
+
+export const Content = styled.p``
+
---
+++ src/components/Accordion/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Container, Inner, Frame, Header, Title, Toggle, Body, Content } from './_Accordion.styles'
+
+export default function Accordion ({ children, ...restProps }) {
+	return <Container { ...restProps }> { children } </Container>
+}
+
+Accordion.Inner = function AccordionInner ({ children, ...restProps }) {
+	return <Inner { ...restProps }> { children } </Inner>
+}
+
+Accordion.Frame = function AccordionFrame ({ children, ...restProps }) {
+	return <Frame { ...restProps }> { children } </Frame>
+}
+
+Accordion.Header = function AccordionHeader ({ children, ...restProps }) {
+	return <Header { ...restProps }> { children } </Header>
+}
+
+Accordion.Title = function AccordionTitle ({ children, ...restProps }) {
+	return <Title { ...restProps }> { children } </Title>
+}
+
+Accordion.Toggle = function AccordionToggle ({ children, ...restProps }) {
+	return <Toggle { ...restProps }> { children } </Toggle>
+}
+
+Accordion.Body = function AccordionBody ({ children, ...restProps }) {
+	return <Body { ...restProps }> { children } </Body>
+}
+
+Accordion.Content = function AccordionContent ({ children, ...restProps }) {
+	return <Content { ...restProps }> { children } </Content>
+}
+
